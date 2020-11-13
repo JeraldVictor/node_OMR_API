00ae95241d0794e6389e05997100cf013c38ffb3
JeraldVictor <jeraldvictor123@yahoo.com>
2020-11-13 19:47:38 +0530

    dynamic require for squelize models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require("sequelize");
 const config = require("../configs");
+const path = require("path");
+const fs = require("fs");
 
 const sequelize = new Sequelize(
   config.db.database,
@@ -7,14 +9,19 @@ const sequelize = new Sequelize(
   config.db.password,
   config.db.options
 );
-
-//! import Models
-
-const db = {
+let db = {
   sequelize,
   Sequelize,
-  User: require("./User")(sequelize, Sequelize),
-  Session: require("./Session")(sequelize, Sequelize),
 };
+
+//! import Models .js files
+fs.readdirSync(__dirname)
+  .filter((file) => file !== "index.js")
+  .forEach((file) => {
+    db[path.basename(file, ".js")] = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize
+    );
+  });
 
 module.exports = db;
